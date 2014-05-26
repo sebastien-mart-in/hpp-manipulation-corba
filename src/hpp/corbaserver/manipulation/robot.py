902a182666bdfef9bdb0a27dd57601299199534d
Florent Lamiraux <florent@laas.fr>
2014-05-26 14:22:38 +0200

    Add tf_root member in python class Robot for proper display in rviz.

--- src/hpp/corbaserver/manipulation/robot.py
+++ src/hpp/corbaserver/manipulation/robot.py
@@ -39,6 +39,7 @@ class CorbaClient:
 #  chains rooted at an anchor joint.
 class Robot (object):
     def __init__ (self, robotName, rootJointType):
+        self.tf_root = "base_link"
         self.rootJointType = rootJointType
         self.robotName = robotName
         self.client = CorbaClient ()
