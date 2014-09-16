996d139c5ccca3515df17c71f435cc0599567381
Joseph Mirabel <jmirabel@laas.fr>
2014-09-16 11:31:22 +0200

    Upgrade pregrasp task

--- src/problem.impl.cc
+++ src/problem.impl.cc
@@ -95,9 +95,18 @@ namespace hpp {
 	try {
 	  const GripperPtr_t gripper = robot->gripper (gripperName);
 	  const HandlePtr_t& handle = robot->handle (handleName);
+          std::string name (graspName);
 	  DifferentiableFunctionPtr_t constraint =
 	    handle->createPreGrasp (gripper);
+	  DifferentiableFunctionPtr_t ineq_positive =
+	    handle->createPreGraspOnLine (gripper, 0);
+	  DifferentiableFunctionPtr_t ineq_negative =
+	    handle->createPreGraspOnLine (gripper, 0.1);
 	  problemSolver_->addNumericalConstraint (graspName, constraint);
+	  problemSolver_->addNumericalConstraint (name + "/ineq_0", ineq_positive);
+	  problemSolver_->addInequalityVector (name + "/ineq_0", - vector_t::Ones(1));
+	  problemSolver_->addNumericalConstraint (name + "/ineq_0.1", ineq_negative);
+	  problemSolver_->addInequalityVector (name + "/ineq_0.1", vector_t::Ones(1));
           problemSolver_->addGrasp(constraint, gripper, handle);
 	} catch (const std::exception& exc) {
 	  throw Error (exc.what ());
