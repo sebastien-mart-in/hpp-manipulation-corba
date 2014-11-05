b6686b3db44de5b61c745b2c3bd4d19270868884
Joseph Mirabel <jmirabel@laas.fr>
2014-11-05 15:07:23 +0100

    Extend Python class ConstraintGraph

--- src/hpp/corbaserver/manipulation/constraint_graph.py
+++ src/hpp/corbaserver/manipulation/constraint_graph.py
@@ -25,9 +25,63 @@
 #  Some method implemented by the server can be considered as private. The
 #  goal of this class is to hide them and to expose those that can be
 #  considered as public.
-class ConstraintGraph (object):
-    def __init__ (self, robot):
-        self.client = robot.client
 
-    def display ():
-        self.client.manipulation.graph.display ()
+from subprocess import Popen
+
+class ConstraintGraph (object):
+    dotCmd = ['dot', '-Tpdf']
+    pdfviewCmd = ['evince']
+
+    def __init__ (self, robot, graphName):
+        self.client = robot.client.manipulation
+        self.clientBasic = robot.client.basic
+        self.graph = robot.client.manipulation.graph
+        self.name = graphName
+        self.nodes = dict ()
+        self.edges = dict ()
+        self.graphId = self.graph.createGraph (graphName)
+        self.subGraphId = self.graph.createSubGraph (graphName + "_sg")
+
+    def display (self, dotOut = '/tmp/constraintgraph.dot', pdfOut = '/tmp/constraintgraph.pdf'):
+        self.client.manipulation.graph.display (dotOut)
+        dotCmd = self.dotCmd[:]
+        dotCmd.append ('-o' + dotOut)
+        dotCmd.append (pdfOut)
+        dot = Popen (self.dotCmd)
+        dot.wait ()
+        pdfviewCmd = self.pdfviewCmd[:]
+        pdfviewCmd.append (pdfOut)
+        Popen (pdfviewCmd)
+
+    def createNode (self, node)
+        '''
+        Create one node if node is a string and several if node is a list of string.
+        The order is important. The first should be the most restrictive one as a configuration
+        will be in the first node for which the constraint are satisfied.
+        '''
+        if type (node) is str:
+            node = [node]
+        for n in node:
+            self.nodes [n] = self.graph.createNode (self.subGraphId, n)
+
+    def createEdge (self, nodeFrom, nodeTo, name, weight = 1, isInNodeFrom = None):
+        if isInNodeFrom is None:
+            isInNodeFrom = (self.nodes[nodeFrom] > self.nodes[nodeTo])
+        self.edges [name] =\
+            self.graph.createEdge (self.nodes[nodeFrom], self.nodes[nodeTo], name, weight, isInNodeFrom)
+
+    def createWaypointEdge (self, nodeFrom, nodeTo, name, nb = 1, weight = 1, isInNodeFrom = None):
+        if isInNodeFrom is None:
+            isInNodeFrom = (self.nodes[nodeFrom] > self.nodes[nodeTo])
+        elmts = self.graph.createWaypointEdge (self.nodes[nodeFrom], self.nodes[nodeTo], name, nb, weight, isInNodeFrom)
+        for e in elmts.edges:
+            self.edges [e.name] = e.id
+        for n in elmts.nodes:
+            self.nodes [n.name] = n.id
+        return elmts
+
+    def createGrasp (self, name, gripper, handle, passiveJoints = None):
+        self.client.problem.createGrasp (name, gripper, handle)
+        if passiveJoints is not None:
+            self.client.problem.createGrasp (name + "_passive", gripper, handle)
+            self.clientBasic.problem.setPassiveDofs (name + "_passive", passiveJoints)
