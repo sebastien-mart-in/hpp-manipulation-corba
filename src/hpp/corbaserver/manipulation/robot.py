d516100a50c9b3d048f8d164743281c1a52ec586
Florent Lamiraux <florent@laas.fr>
2014-12-22 09:06:02 +0100

    Forward some more methods from python corbaserver.Robot to manipulation.Robot.

--- src/hpp/corbaserver/manipulation/robot.py
+++ src/hpp/corbaserver/manipulation/robot.py
@@ -16,6 +16,7 @@
 # hpp-manipulation-corba.  If not, see
 # <http://www.gnu.org/licenses/>.
 
+from hpp import Transform
 from hpp.corbaserver.manipulation import Client as ManipulationClient
 from hpp.corbaserver.wholebody_step import Client as WholebodyStepClient
 from hpp.corbaserver import Client as BasicClient
@@ -158,6 +159,10 @@ class Robot (object):
     def getJointNames (self):
         return self.client.basic.robot.getJointNames ()
 
+    ## Get joint names in the same order as in the configuration.
+    def getAllJointNames (self):
+        return self.client.basic.robot.getAllJointNames ()
+
     ## Get joint position.
     def getJointPosition (self, jointName):
         return self.client.basic.robot.getJointPosition (jointName)
@@ -205,6 +210,44 @@ class Robot (object):
     #  \return dofArray Array of degrees of freedom.
     def shootRandomConfig(self):
         return self.client.basic.robot.shootRandomConfig ()
+
+    ## \}
+
+    ## \name Bodies
+    #  \{
+
+    ##  Get the list of objects attached to a joint.
+    #  \param inJointName name of the joint.
+    #  \return list of names of CollisionObject attached to the body.
+    def getJointInnerObjects (self, jointName):
+        return self.client.basic.robot.getJointInnerObjects (jointName)
+
+
+    ##  Get list of collision objects tested with the body attached to a joint
+    #  \param inJointName name of the joint.
+    #  \return list of names of CollisionObject
+    def getJointOuterObjects (self, jointName):
+        return self.client.basic.robot.getJointOuterObjects (jointName)
+
+    ## Get position of robot object
+    #  \param objectName name of the object.
+    #  \return transformation as a hpp.Transform object
+    def getObjectPosition (self, objectName):
+        return Transform (self.client.basic.robot.getObjectPosition
+                          (objectName))
+
+    ## \brief Remove an obstacle from outer objects of a joint body
+    #
+    #  \param objectName name of the object to remove,
+    #  \param jointName name of the joint owning the body,
+    #  \param collision whether collision with object should be computed,
+    #  \param distance whether distance to object should be computed.
+    def removeObstacleFromJoint (self, objectName, jointName, collision,
+                                 distance):
+        return self.client.basic.obstacle.removeObstacleFromJoint \
+            (objectName, jointName, collision, distance)
+
+
     ## \}
 
     ## \name Collision checking and distance computation
@@ -215,8 +258,18 @@ class Robot (object):
     # Check whether current configuration of robot is valid by calling
     # CkwsDevice::collisionTest ().
     # \return whether configuration is valid
+    # \note Deprecated. Use isConfigValid instead.
     def collisionTest (self):
+        print "Deprecated. Use isConfigValid instead"
         return self.client.basic.robot.collisionTest ()
+
+    ## Check the validity of a configuration.
+    #
+    # Check whether a configuration of robot is valid.
+    # \param cfg a configuration
+    # \return whether configuration is valid
+    def isConfigValid (self, cfg):
+        return self.client.basic.robot.isConfigValid (cfg)
 
     ## Compute distances between bodies and obstacles
     #
@@ -236,15 +289,15 @@ class Robot (object):
     # \{
 
     ## Get mass of robot
-    def getMass ():
-        return self.client.robot.getMass ()
+    def getMass (self):
+        return self.client.basic.robot.getMass ()
 
     ## Get position of center of mass
-    def getCenterOfMass ():
-        return self.client.robot.getCenterOfMass ()
+    def getCenterOfMass (self):
+        return self.client.basic.robot.getCenterOfMass ()
     ## Get Jacobian of the center of mass
-    def getJacobianCenterOfMass ():
-        return self.client.robot.getJacobianCenterOfMass ()
+    def getJacobianCenterOfMass (self):
+        return self.client.basic.robot.getJacobianCenterOfMass ()
     ##\}
 
 class HumanoidRobot (Robot):
