1ad4e8c125d99481334ef0c4e39a70a99b2f7cbd
Joseph Mirabel <jmirabel@laas.fr>
2019-05-16 18:37:05 +0200

    Update Robot class.

--- src/hpp/corbaserver/manipulation/__init__.py
+++ src/hpp/corbaserver/manipulation/__init__.py
@@ -5,6 +5,7 @@ from .problem_solver import ProblemSolver, newProblem
 from .constraint_graph import ConstraintGraph
 from .constraint_graph_factory import ConstraintGraphFactory
 from .constraints import Constraints
-from .robot import CorbaClient
+from .robot import CorbaClient, Robot
+from hpp.corbaserver import loadServerPlugin, createContext
 
 from hpp_idl.hpp.corbaserver.manipulation import Rule
